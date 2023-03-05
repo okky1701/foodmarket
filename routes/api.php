aadde02eaf347eda5b7f0459319bba0b6802772b
Abbas Ishaq <okky1701@gmail.com>
2023-03-05 13:46:41 +0700

    API Food and Transaction

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,22 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\FoodController;
+use App\Http\Controllers\API\TransactionController;
+use App\Http\Controllers\API\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('user', [UserController::class, 'fetch']);
+    Route::post('user', [UserController::class, 'updateProfile']);
+    Route::post('user/photo', [UserController::class, 'updatePhoto']);
+    Route::post('logout', [UserController::class, 'logout']);
+
+    Route::get('transaction',[TransactionController::class, 'all']);
 });
+
+
+Route::post('login',[UserController::class,'login']);
+Route::post('register',[UserController::class,'register']);
+
+Route::get('food', [FoodController::class, 'all']);
